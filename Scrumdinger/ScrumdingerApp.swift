91a7ccf63256d5b17e612e9e809d17beb72da6b8
Dominik Kreslo <dominik061997@gmail.com>
2021-07-13 20:59:20 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Dominik Kreslo on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
